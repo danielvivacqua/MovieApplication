e5c23c138ce29389a031302d908ef8a0466d5e4e
Your Name <you@example.com>
2016-05-17 16:34:21 -0400

    Movie Application using MVC

---
+++ MovieApplication/Models/BuddyClasses/CustomerBuddy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApplication.Models
+{
+
+    [MetadataType(typeof(CustomerBuddy))]
+    public partial class Customer
+    {
+        public string FullName
+        {
+            get { return this.FirstName + " " + this.LastName; }
+            set { }
+        }
+    }
+    sealed class CustomerBuddy
+    {
+        
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        
+        [Display(Name ="Last Name")]
+       // [Required]
+        public string LastName { get; set; }
+
+        [Display(Name ="Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ MovieApplication/Models/BuddyClasses/MovyBuddy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApplication.Models
+{
+    [MetadataType(typeof(MovyBuddy))]
+    public partial class Movy
+    {
+
+    }
+
+    public class MovyBuddy
+    {
+        public int MovieID { get; set; }
+
+        [Display(Name = "The Title")]
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
